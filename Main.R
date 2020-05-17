f997701a18e533616faf83d44782e3762de8fdb3
Antonio Jose Castaneda Gomez <antonio.castanedag@gmail.com>
2020-05-18 00:10:12 +0200

    Basic graphs

--- Main.R
+++ Main.R
@@ -1,20 +1,30 @@
+######################## LOAD LIBRARIES 
 library(ggplot2)
 library(gganimate)
 library(ggmap)
 library(sf)
 require(pals)
 library(ggdark)
+require(reshape2)
+library(RColorBrewer)
 
+######################## PREPARE THE DATA 
 getwd()
 setwd("Data")
 file <- "TEST.TXT"
 Data <- read.table(file, header = FALSE, sep = ",", dec = ".")
-names(Data) <- c("ID","LON","LAT","ALT","TIME","DATE","SAT","SPEED")
+names(Data) <- c("ID","LAT","LON","ALT","TIME","DATE","SAT","SPEED")
 
-Data <- Data[(Data$LON > 49.77 & Data$LON < 49.805),]
-Data <- Data[(Data$LAT > 9.889 & Data$LAT < 9.95),]
+######################## STORE AND DELETE OUTLINERS 
+LongOut <- boxplot(Data$LON)$out
+LatgOut <- boxplot(Data$LAT)$out
 
-base <- ggplot(Data, aes(x=LAT, y=LON, color=SPEED))+
+Data <- Data[!Data$LON %in% LongOut,]
+Data <- Data[!Data$LAT %in% LatgOut,]
+
+Data$ID <- seq(1:length(Data$ID))
+######################## PLOT DATA
+Plot <- ggplot(Data, aes(x=LON, y=LAT, color=SPEED))+
   geom_point() +
   geom_path(lineend = "round") +
   coord_fixed(ratio = 1) +
@@ -24,14 +34,25 @@ base <- ggplot(Data, aes(x=LAT, y=LON, color=SPEED))+
        subtitle = "Date: 17/05/20",
        x = "Longitude",
        y = "Latitude",
-       color = "Speed m/s")+
-  transition_reveal(along = TIME)
+       color = "Speed m/s")
 
+######################## MAKE GIF
+Gif <- Plot + transition_reveal(along = ID)
 animate(base, fps = 10, width = 694, height = 403)
-
 anim_save("Test.gif")
 
+######################## PLOT DENSITY TO CHECK SLOW POINTS
+ggplot(Data, aes(x=LON, y=LAT) ) +
+  geom_bin2d(bins = 200) +
+  dark_theme_gray() +
+  scale_fill_continuous(type = "viridis") +
+  labs(title = "GPS DENSITY",
+       subtitle = "Date: 17/05/20",
+       x = "Longitude",
+       y = "Latitude",
+       color = "Bins")
 
+######################## EXPORT GEOPACKAGE
 p.sf <- st_as_sf(Data, coords = c("LAT", "LON"), crs = 4326) 
 st_write(p.sf, "Points_test.gpkg", driver="GPKG")  # Create a geopackage file
 
